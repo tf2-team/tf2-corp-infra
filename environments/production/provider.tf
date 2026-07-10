61d2bcc783163092652b480b24e70c0f1b950b39
TranManhCuong <ctran13904@gmail.com>
2026-07-10 21:51:50 +0700

    fix: resolve EKS provider Unauthorized error using exec plugin in both environments
    - Replace data.aws_eks_cluster_auth with dynamic exec authentication in kubernetes and helm providers
    - Applies to both dev and production provider configurations

--- environments/production/provider.tf
+++ environments/production/provider.tf
@@ -39,21 +39,24 @@ provider "aws" {
   }
 }
 
-# EKS auth for optional in-cluster installs (Argo CD). Requires cluster reachable at apply time when argocd_enabled=true.
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
-}
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
 }
 
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.this.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
   }
 }
